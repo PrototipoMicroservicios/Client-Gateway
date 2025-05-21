d4b1ca266a399782bf4beaf3b374b8d6a717a9c6
RicardoTipan <ricardojaviertipan@gmail.com>
2025-05-20 22:07:06 -0500

    Grabar Ordenes/ detalle en base de datos y retonar informacion

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,26 +1,14 @@
-import { IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive } from "class-validator";
-import { OrderStatus, OrderStatusList } from "../enum/order.enum";
+import { ArrayMinSize, IsArray, IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive, ValidateNested } from "class-validator";
+import { OrderStatusList } from "../enum/order.enum";
+import { Type } from "class-transformer";
+import { OrderItemDto } from "./order-item.dto";
 
 export class CreateOrderDto {
-
-
-    @IsNumber()
-    @IsPositive()
-    totalAmount: number;
-
-    @IsNumber()
-    @IsPositive()
-    totalItems: number;
-
-    @IsEnum(OrderStatusList,{
-        message: `Possible status values are ${OrderStatusList}`
-    })
-    @IsOptional()
-    status: OrderStatus = OrderStatus.PENDING
-
-    @IsOptional()
-    @IsBoolean()
-    paid: boolean = false;
-
+    
+@IsArray()
+@ArrayMinSize(1)
+@ValidateNested({each:true})
+@Type(() => OrderItemDto)
+items: OrderItemDto[]
 
 }
